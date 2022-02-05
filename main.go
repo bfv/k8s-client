acc0d3c8dabc3af4769fe9db1a1b9e2256297e16
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2022-02-05 11:29:30 +0100

    counting pods in cluster

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.Println("Hello world!")
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		time.Sleep(10 * time.Second)
+	}
+
 }
